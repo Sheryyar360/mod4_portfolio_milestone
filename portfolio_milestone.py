b4c7007bcb21897cf3dd5b6ea6d51c783729c834
Sheheryar Saeed <sheryyar36@gmail.com>
2024-06-09 14:27:00 -0400

    portfolio milestone complete

--- portfolio_milestone.py
+++ portfolio_milestone.py
@@ -15,8 +15,10 @@ product2.item_name = input('Enter name of product 2: ')
 product2.item_price = float(input('Enter price of product 2: '))
 product2.item_quantity = int(input('Enter quantity of product 2: '))
 
-product1_total = product1.item_quantity * product1.item_price
-product2_total = product2.item_quantity * product2.item_price
-print('{} {} @ ${:.2f} = ${:.2f}'.format(product1.item_quantity, product1.item_name, product1.item_price, product1_total ))
-print('{} {} @ ${:.2f} = ${:.2f}'.format(product2.item_quantity, product2.item_name, product2.item_price, product2_total ))
-print('Grand total = ${:.2f}'.format(product1_total + product2_total))
+def print_item_cost(product):
+    product_total = product.item_quantity * product.item_price
+    return product_total
+
+print('{} {} @ ${:.2f} = ${:.2f}'.format(product1.item_quantity, product1.item_name, product1.item_price, print_item_cost(product1)))
+print('{} {} @ ${:.2f} = ${:.2f}'.format(product2.item_quantity, product2.item_name, product2.item_price, print_item_cost(product2)))
+print('Grand total = ${:.2f}'.format((print_item_cost(product1) + print_item_cost(product2))))
