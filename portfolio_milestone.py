d8058908d638b6edb7ee12401ed0d0e80b89964a
Sheheryar Saeed <Sheryyar36@gmail.com>
2024-06-23 10:53:08 -0400

    renamed methods

--- portfolio_milestone.py
+++ portfolio_milestone.py
@@ -14,10 +14,10 @@ product2.item_name = input('Enter name of product 2: ')
 product2.item_price = float(input('Enter price of product 2: '))
 product2.item_quantity = int(input('Enter quantity of product 2: '))
 
-def print_item_cost(product):
+def item_cost(product):
     product_total = product.item_quantity * product.item_price
     return product_total
 
-print('{} {} @ ${:.2f} = ${:.2f}'.format(product1.item_quantity, product1.item_name, product1.item_price, print_item_cost(product1)))
-print('{} {} @ ${:.2f} = ${:.2f}'.format(product2.item_quantity, product2.item_name, product2.item_price, print_item_cost(product2)))
-print('Grand total = ${:.2f}'.format((print_item_cost(product1) + print_item_cost(product2))))
+print('{} {} @ ${:.2f} = ${:.2f}'.format(product1.item_quantity, product1.item_name, product1.item_price, item_cost(product1)))
+print('{} {} @ ${:.2f} = ${:.2f}'.format(product2.item_quantity, product2.item_name, product2.item_price, item_cost(product2)))
+print('Grand total = ${:.2f}'.format((item_cost(product1) + item_cost(product2))))
